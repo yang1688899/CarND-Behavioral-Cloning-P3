eb39c18709bd498a9d5046e72ee8dc2a9641360e
yang1688899 <yang535460548484@gamil.com>
2017-09-15 00:41:46 +0800

    commit

--- train.py
+++ train.py
@@ -13,6 +13,47 @@ from keras.layers.core import Dense,Flatten,Activation,Lambda
 from keras.layers.convolutional import Conv2D
 from keras.layers.pooling import MaxPooling2D
 from keras.layers import Cropping2D
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+
+#define a generator
+def generator(samples, batch_size=32):
+    samples_size = len(samples)
+    while 1:
+        shuffle(samples)
+        for offset in (0,batch_size,samples_size):
+            batch_samples = samples[offset:offset+batch_size]
+            
+            imgs = []
+            measurements = []
+            
+        
+
+        for sample in batch_samples:
+            current_center_paths = [get_current_path(path) for path in sample[:3]]
+            img_samples = [cv2.imread(path) for path in current_center_paths]
+            
+            measurement = float(line[3])
+            correction = 0.2
+            measurement_left = measurement + correction
+            measurement_right = measurement - correction
+            measurement_samples = [measurement,measurement_left,measurement_right]
+            
+            imgs.extend(img_samples)
+            measurements.extend(measurement_samples)
+
+    
+        for i in range(len(imgs)):
+            image_flipped = np.fliplr(imgs[i])
+            measurement_flipped = -measurements[i]
+            imgs.append(image_flipped)
+            measurements.append(measurement_flipped)
+    
+    
+        X_train = np.array(imgs)
+        y_train = np.array(measurements)
+        print(X_train.shape[1:])
+        yield X_train,y_train
  
 def get_current_path(path):
     fileName = path.split('\\')[-1]
@@ -25,42 +66,16 @@ with open('../my_data/driving_log.csv') as csvFile:
     next(reader)
     for line in reader:
         lines.append(line)
+#split the train and validate set        
+train_samples, validate_samples = train_test_split(lines,test_size=0.2)
+
+train_generator = generator(train_samples,batch_size=256)
+validate_generator = generator(validate_samples,batch_size=256)
         
-    
-imgs = []
-measurements = []
-for line in lines:
 
-    current_center_paths = [get_current_path(path) for path in line[:3]]
-    img_samples = [cv2.imread(path) for path in current_center_paths]
-    
-    measurement = float(line[3])
-    correction = 0.2
-    measurement_left = measurement + correction
-    measurement_right = measurement - correction
-    measurement_samples = [measurement,measurement_left,measurement_right]
-    
-    imgs.extend(img_samples)
-    measurements.extend(measurement_samples)
-print(len(imgs))
-print(len(measurements))
-    
-for i in range(len(imgs)):
-    image_flipped = np.fliplr(imgs[i])
-    measurement_flipped = -measurements[i]
-    imgs.append(image_flipped)
-    measurements.append(measurement_flipped)
-    
-print(len(imgs))
-print(len(measurements))
-    
-X_train = np.array(imgs)
-y_train = np.array(measurements)
-
-input_shape = X_train.shape[1:]
 model = Sequential()
 
-model.add(Cropping2D(cropping=((70,25), (0,0)), input_shape=input_shape))
+model.add(Cropping2D(cropping=((70,25), (0,0)), input_shape=[160, 320, 3]))
 model.add(Lambda(lambda x: x/255. - 0.5))
 model.add(Conv2D(filters=6,kernel_size=(5,5)))
 model.add(Activation('relu'))
@@ -80,7 +95,8 @@ model.add(Activation('relu'))
 model.add(Dense(1))
 
 model.compile(optimizer='adam', loss='mse')
-model.fit(X_train,y_train,validation_split=0.2,shuffle=True,epochs=5,batch_size=128)
+model.fit_generator(train_generator,samples_per_epoch=len(train_samples)*6,validation_data=\
+          validate_generator,nb_val_samples=len(validate_samples)*6,nb_epoch=5)
 model.save('model.h5')
 exit()
     
