e8e8f845b46a878981367886e40232f8f7f7281e
yang1688899 <yang535460548484@gamil.com>
2017-09-29 23:15:16 +0800

    change the drive.py

--- drive.py
+++ drive.py
@@ -58,10 +58,16 @@ def telemetry(sid, data):
         # The current speed of the car
         speed = data["speed"]
         # The current image from the center camera of the car
+        # imgString = data["image"]
+        # image = Image.open(BytesIO(base64.b64decode(imgString)))
+        # image_array = np.asarray(image)
+        # steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
+
         imgString = data["image"]
-        image = Image.open(BytesIO(base64.b64decode(imgString)))
-        image_array = np.asarray(image)
-        steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
+      	image = Image.open(BytesIO(base64.b64decode(imgString)))
+      	image_array = np.asarray(image)
+      	image_array = image_array[:,:,::-1]  # this line converts image from RGB to BGR
+      	steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
 
         throttle = controller.update(float(speed))
 
